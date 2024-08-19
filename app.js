8a88ad301ba42808b526be8c18780b5a4d8a04d6
Ben Klimchock <klimmy1115@gmail.com>
2024-08-19 15:06:34 -0500

    Size of hit box for player

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ window.addEventListener("load", function(){
             this.game = game;
             this.collisionX = this.game.width * 0.5;
             this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 30;
         }
         
         draw(context) {
             context.beginPath();
-            context.arc(this.collisionX, this.collisionY, 50, 0, Math.PI * 2);
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
             context.fill();
         }
     }
